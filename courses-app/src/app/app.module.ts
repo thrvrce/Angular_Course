995bcd8649f3af0a99eaa05b868e7d2d13836c7c
Viktar Audzeyeu <viktar_audzeyeu@epam.com>
2023-03-26 16:48:03 +0400

    Task 5.1 Create initial boilerplate

--- courses-app/src/app/app.module.ts
+++ courses-app/src/app/app.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoursesModule } from './features/courses/courses.module';
-
 import { SharedModule } from './shared/shared.module';
-
 import { AuthModule } from './auth/auth.module';
-
 import { AuthService } from './auth/services/authService/auth.service';
 import { AuthorizedGuard } from './auth/guards/authorizedGuard/authorized.guard';
 import { NotAuthorizedGuard } from './auth/guards/notAuthorizedGuard/not-authorized.guard';
@@ -18,6 +18,8 @@ import { CoursesService } from './services/courses/courses.service';
 import { CoursesStoreService } from './services/coursesStore/courses-store.service';
 import { UserService } from './user/service/userService/user.service';
 import { UserStoreService } from './user/service/useStore/user-store.service';
+import { reducers, effects } from './store';
+
 @NgModule({
   declarations: [AppComponent],
   imports: [
@@ -26,7 +28,18 @@ import { UserStoreService } from './user/service/useStore/user-store.service';
     CoursesModule,
     SharedModule,
     HttpClientModule,
-    AuthModule
+    AuthModule,
+    EffectsModule.forRoot(effects),
+    StoreModule.forRoot(reducers),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: false,
+      features: {
+        pause: false,
+        lock: true,
+        persist: true
+      }
+    })
   ],
   providers: [
     AuthService,
---
+++ courses-app/src/app/store/index.ts
@@ -0,0 +1,5 @@
+import { ActionReducerMap } from "@ngrx/store";
+
+interface State {}
+export const reducers: ActionReducerMap<State> = {}
+export const effects = []
